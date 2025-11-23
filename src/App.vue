a80069bfad663236a219ed28ae20834d2c048eeb
Paul Dobrick <paul.dobrick@outlook.de>
2025-11-23 14:02:10 +0100

    Initial commit: Portfolio setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="outline">
+    
+  </div>
+</template>
+
+<style scoped>
+.outline {
+  width: 100vw;
+  height: 100vh;
+  background-color: #991b1b;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="outline-navbar">
+
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+    setup() {
+
+    return {
+
+    }
+    }
+})
+</script>
+
+<style scoped lang="scss">
+
+.outline-navbar {
+
+}
+
+</style>
